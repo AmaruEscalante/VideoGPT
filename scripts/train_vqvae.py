c4eb61196e4c771fe5dcdbbeb7d3c2c3e988472a
AmaruEscalante <amaruescalante@gmail.com>
2023-11-11 18:34:28 -0800

    fix: pytorch lighting needs version =<2

--- scripts/train_vqvae.py
+++ scripts/train_vqvae.py
@@ -10,11 +10,13 @@ def main():
     parser = argparse.ArgumentParser()
     parser = pl.Trainer.add_argparse_args(parser)
     parser = VQVAE.add_model_specific_args(parser)
-    parser.add_argument('--data_path', type=str, default='/home/wilson/data/datasets/bair.hdf5')
-    parser.add_argument('--sequence_length', type=int, default=16)
-    parser.add_argument('--resolution', type=int, default=64)
-    parser.add_argument('--batch_size', type=int, default=32)
-    parser.add_argument('--num_workers', type=int, default=8)
+    parser.add_argument(
+        "--data_path", type=str, default="/home/wilson/data/datasets/bair.hdf5"
+    )
+    parser.add_argument("--sequence_length", type=int, default=16)
+    parser.add_argument("--resolution", type=int, default=64)
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--num_workers", type=int, default=8)
     args = parser.parse_args()
 
     data = VideoData(args)
@@ -24,17 +26,17 @@ def main():
     model = VQVAE(args)
 
     callbacks = []
-    callbacks.append(ModelCheckpoint(monitor='val/recon_loss', mode='min'))
+    callbacks.append(ModelCheckpoint(monitor="val/recon_loss", mode="min"))
 
     kwargs = dict()
     if args.gpus > 1:
-        kwargs = dict(distributed_backend='ddp', gpus=args.gpus)
-    trainer = pl.Trainer.from_argparse_args(args, callbacks=callbacks,
-                                            max_steps=200000, **kwargs)
+        kwargs = dict(distributed_backend="ddp", gpus=args.gpus)
+    trainer = pl.Trainer.from_argparse_args(
+        args, callbacks=callbacks, max_steps=200000, **kwargs
+    )
 
     trainer.fit(model, data)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
