886226f5e8fffde54da34e94f36ba8e7f488757c
AmaruEscalante <amaruescalante@gmail.com>
2023-11-14 19:08:00 -0800

    feat: add .fvd funcs to init in main module

--- videogpt/__init__.py
+++ videogpt/__init__.py
@@ -1,6 +1,5 @@
-
 from .vqvae import VQVAE
 from .gpt import VideoGPT
 from .data import VideoData
 from .download import load_vqvae, load_videogpt, load_i3d_pretrained, download
-
+from .fvd.fvd import get_fvd_logits, frechet_distance
