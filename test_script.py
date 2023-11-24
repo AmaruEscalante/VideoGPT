5411d04790bbc38a38ba486fedc63992c498d3c7
AmaruEscalante <amaruescalante@gmail.com>
2023-11-23 19:15:04 -0800

    feat: script supports args

--- test_script.py
+++ test_script.py
@@ -1,15 +1,24 @@
+import argparse
 from videogpt.gpt import VideoGPT
 from videogpt import download, load_vqvae, load_videogpt
 from videogpt.vqvae import VQVAE
 from videogpt.download import load_i3d_pretrained
-from tqdm import tqdm  # this is for script version
-
+from tqdm import tqdm
 import numpy as np
 import torch
-import torch.distributed as dist
-
 from videogpt.fvd.fvd import get_fvd_logits, frechet_distance
 from videogpt import VideoData, VideoGPT, load_videogpt
+
+# Initialize argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument("--trials", type=int, default=1, help="Number of trials to run")
+parser.add_argument(
+    "--batch_size", type=int, default=16, help="Batch size for processing"
+)
+parser.add_argument(
+    "--max_batch", type=int, default=32, help="Maximum batch size for processing"
+)
+args = parser.parse_args()
 
 # Download VQ-VAE
 device = torch.device("cuda")
@@ -20,7 +29,7 @@ filepath = download("1c4CYL1joN5KDC5VYJIilFYWcDOmjWtgE", "ucf101_uncond_gpt")
 gpt = VideoGPT.load_from_checkpoint(filepath).to(device)
 
 
-def main(ckpt="bair_gpt", n_trials=1, batch_size=16):
+def main(ckpt="bair_gpt", n_trials=1, batch_size=16, max_batch=32):
     torch.set_grad_enabled(False)
     #################### Load VideoGPT ########################################
     gpt.eval()
@@ -37,7 +46,7 @@ def main(ckpt="bair_gpt", n_trials=1, batch_size=16):
     fvds_star = []
     pbar = tqdm(total=n_trials)
     for _ in range(n_trials):
-        fvd, fvd_star = eval_fvd(i3d, gpt, loader, device)
+        fvd, fvd_star = eval_fvd(i3d, gpt, loader, device, max_batch)
         fvds.append(fvd)
         fvds_star.append(fvd_star)
 
@@ -57,21 +66,14 @@ def main(ckpt="bair_gpt", n_trials=1, batch_size=16):
     )
 
 
-def all_gather(tensor):
-    rank, size = dist.get_rank(), dist.get_world_size()
-    tensor_list = [torch.zeros_like(tensor) for _ in range(size)]
-    dist.all_gather(tensor_list, tensor)
-    return torch.cat(tensor_list)
-
-
-def eval_fvd(i3d, videogpt, loader, device):
+def eval_fvd(i3d, videogpt, loader, device, max_batch):
     batch = next(iter(loader))
     batch = {k: v.to(device) for k, v in batch.items()}
 
     fake_embeddings = []
-    for i in range(0, batch["video"].shape[0], MAX_BATCH):
+    for i in range(0, batch["video"].shape[0], max_batch):
         fake = videogpt.sample(
-            MAX_BATCH, {k: v[i : i + MAX_BATCH] for k, v in batch.items()}
+            max_batch, {k: v[i : i + max_batch] for k, v in batch.items()}
         )
         fake = torch.repeat_interleave(fake, 4, dim=2)  # TODO: check correctness
         fake = fake.permute(0, 2, 3, 4, 1).cpu().numpy()  # BCTHW -> BTHWC
@@ -81,9 +83,9 @@ def eval_fvd(i3d, videogpt, loader, device):
 
     real = batch["video"].to(device)
     real_recon_embeddings = []
-    for i in range(0, batch["video"].shape[0], MAX_BATCH):
+    for i in range(0, batch["video"].shape[0], max_batch):
         real_recon = (
-            videogpt.get_reconstruction(batch["video"][i : i + MAX_BATCH]) + 0.5
+            videogpt.get_reconstruction(batch["video"][i : i + max_batch]) + 0.5
         ).clamp(0, 1)
         real_recon = torch.repeat_interleave(real_recon, 4, dim=2)
         real_recon = real_recon.permute(0, 2, 3, 4, 1).cpu().numpy()
@@ -107,5 +109,10 @@ def eval_fvd(i3d, videogpt, loader, device):
     return fvd.item(), fvd_star.item()
 
 
-MAX_BATCH = 16
-main(ckpt="ufc", n_trials=5, batch_size=16)
+if __name__ == "__main__":
+    main(
+        ckpt="ufc",
+        n_trials=args.trials,
+        batch_size=args.batch_size,
+        max_batch=args.max_batch,
+    )
