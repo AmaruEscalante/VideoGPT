974cb707cb29d3b36c420eef3ed6f45bb6be9bb1
AmaruEscalante <amaruescalante@gmail.com>
2023-11-23 18:55:27 -0800

    fix: unrar script

--- scripts/preprocess/ucf101/create_ucf_dataset.sh
+++ scripts/preprocess/ucf101/create_ucf_dataset.sh
@@ -1,10 +1,30 @@
-#! /bin/sh
+#!/bin/sh
 
 mkdir -p ${1}
 
+# Function to check if unrar is installed and install it locally if not
+install_unrar_if_needed() {
+    if ! command -v unrar &> /dev/null; then
+        echo "unrar not found, installing locally..."
+        wget https://www.rarlab.com/rar/unrarsrc-6.1.7.tar.gz
+        tar -xzvf unrarsrc-6.1.7.tar.gz
+        cd unrarsrc-6.1.7
+        make
+        cd ..
+        export UNRAR="$PWD/unrarsrc-6.1.7/unrar"
+    else
+        export UNRAR="unrar"
+    fi
+}
+
+# Install unrar locally if needed
+install_unrar_if_needed
+
 # Download UCF-101 video files
 wget --no-check-certificate -P ${1} https://www.crcv.ucf.edu/data/UCF101/UCF101.rar
-unrar x ${1}/UCF101.rar ${1}
+
+# Unrar UCF-101 dataset
+$UNRAR x ${1}/UCF101.rar ${1}
 rm ${1}/UCF101.rar
 
 # Download UCF-101 train/test splits
